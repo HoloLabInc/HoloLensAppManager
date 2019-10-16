5bddc6c20d8f23367893487d374a27037c1c9dca
Cindy Chen <cindychen0204@hololab.co.jp>
2019-10-16 18:23:05 +0900

    ダミーUpload

---
+++ HoloLensAppManager/Models/DummyUploader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HoloLensAppManager.Models
+{
+    class DummyUploader : IUploader
+    {
+        public Task<Application> Download(string appName, string version, bool useCache = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppInfo>> GetAppInfoListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Upload(Application application)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
