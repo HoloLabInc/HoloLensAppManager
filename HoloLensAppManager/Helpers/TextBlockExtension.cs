2983eb0c8bf3d89ddf6f79dc249791fcb85475d6
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2020-09-06 21:02:07 +0900

    Text を選択可能に、HTTP リンクを有効に

---
+++ HoloLensAppManager/Helpers/TextBlockExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
+
+namespace HoloLensAppManager.Helpers
+{
+    // ref: https://stackoverflow.com/questions/27734084/create-hyperlink-in-textblock-via-binding
+    public static class TextBlockExtension
+    {
+        public static string GetFormattedText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(FormattedTextProperty);
+        }
+
+        public static void SetFormattedText(DependencyObject obj, string value)
+        {
+            obj.SetValue(FormattedTextProperty, value);
+        }
+
+        public static readonly DependencyProperty FormattedTextProperty =
+            DependencyProperty.Register("FormattedText", typeof(string), typeof(TextBlockExtension),
+            new PropertyMetadata(string.Empty, (sender, e) =>
+            {
+                string text = e.NewValue as string;
+                if (sender is TextBlock textBl)
+                {
+                    textBl.Inlines.Clear();
+                    Regex regx = new Regex(@"(https?://[^\s]+)", RegexOptions.IgnoreCase);
+                    var str = regx.Split(text);
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            textBl.Inlines.Add(new Run { Text = str[i] });
+                        }
+                        else
+                        {
+                            Hyperlink link = new Hyperlink
+                            {
+                                NavigateUri = new Uri(str[i])
+                            };
+                            link.Inlines.Add(new Run { Text = str[i] });
+                            textBl.Inlines.Add(link);
+                        }
+                    }
+                }
+            }));
+    }
+}
